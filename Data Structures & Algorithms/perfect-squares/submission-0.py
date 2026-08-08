a2b8d52a8dacc1a6a57898cda702db638df98c1c
Samarth Jain <samisinthestates@gmail.com>
2026-08-08 18:42:34 -0400

    Add: perfect-squares - submission-0

---
+++ Data Structures & Algorithms/perfect-squares/submission-0.py
@@ -0,0 +1,20 @@
+class Solution:
+    def numSquares(self, n: int) -> int:
+        dp = [-1] * (n+1)
+        def helper(num):
+            if num == 0:
+                return 0
+            
+            if dp[num] >= 0:
+                return dp[num]
+
+            val = int(math.sqrt(num))
+
+            ans = num
+
+            for i in range(val,1,-1):
+                ans = min(ans, 1 + helper(num - i*i))
+            
+            dp[num] = ans
+            return ans
+        return helper(n)
